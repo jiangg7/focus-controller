c8d42b661be6c7762c1aed2b6e704a5591f15307
jiangg7 <103295016+jiangg7@users.noreply.github.com>
2025-12-09 17:09:44 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+// Service Worker for 无线跟焦控制器 PWA
+const CACHE_NAME = 'focus-controller-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 安装 Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// 激活 Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// 拦截网络请求
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果缓存中有，返回缓存
+        if (response) {
+          return response;
+        }
+        // 否则从网络获取
+        return fetch(event.request);
+      })
+  );
+});
